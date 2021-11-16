63d123995baf70eab81655ddfb2cab83a9ed1044
EPPARLA NIKHIL <e.nikhil1999@gmail.com>
2021-11-16 11:02:33 +0530

    http get request

--- lib/Screens/third_screen.dart
+++ lib/Screens/third_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
@@ -8,32 +10,41 @@ class ThirdScreen extends StatefulWidget {
   _FirstScreenState createState() => _FirstScreenState();
 }
 
-String url = 'https://jsonplaceholder.typicode.com/users';
-void feetchdata() async {
-  try {
-    final response = await get(
-      Uri.parse(url),
-    );
-    print(response.body);
-  } catch (e) {
-    print('error');
-  }
-}
-
 class _FirstScreenState extends State<ThirdScreen> {
+  String url = 'https://jsonplaceholder.typicode.com/users';
+  var postJson = [];
+  void feetchdata() async {
+    try {
+      final response = await get(
+        Uri.parse(url),
+      );
+      final jsondata = jsonDecode(response.body) as List;
+
+      print(response.body);
+      setState(() {
+        postJson = jsondata;
+      });
+    } catch (e) {
+      print('error');
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    feetchdata();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          color: Colors.red,
-          height: 30,
-          child: const ElevatedButton(
-            onPressed: feetchdata,
-            child: Text('press here'),
-          ),
-        ),
-      ),
-    );
+        body: ListView.builder(
+      itemCount: postJson.length,
+      itemBuilder: (context, i) {
+        final post = postJson[i];
+        return Text('Id =${post['id']}\n Email =${post['email']}\n\n');
+      },
+    ));
   }
 }
